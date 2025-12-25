59e5ba14e17829c2aec6a219a3d89ce96ff4ccbb
Juan <juan.di176839@alumnos.info.unlp.edu.ar>
2025-12-24 23:17:00 -0300

    chore: fix gh-pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/didesign/',
+  base: '/divdesign/',
   plugins: [react()],
   server: {
     port: 5173,
